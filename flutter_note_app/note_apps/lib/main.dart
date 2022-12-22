b64221db98616d069ae74994edae916b8ea72e3a
notsatria <buanasatriaa@gmail.com>
2022-12-22 21:24:07 +0700

    first commit

---
+++ flutter_note_app/note_apps/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:note_apps/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // themeMode: ThemeMode.dark,
+      theme: ThemeData(
+          primaryColor: Colors.black,
+          scaffoldBackgroundColor: Colors.blueGrey.shade900,
+          appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.transparent, elevation: 0)),
+      title: 'Flutter + PHP CRUD',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Home(),
+      },
+    );
+  }
+}
